bf180149bbc0df72f4ce3b67fa408f2c278265c0
Alex <kyvaevy@gmail.com>
2019-03-25 00:59:37 +0300

    lqst changes

--- src/model/PlayerPosition.ts
+++ src/model/PlayerPosition.ts
@@ -1,6 +1,4 @@
-import { IPositionState } from "../interfaces/PlayerInterfaces";
 import { PositionAbstract as AbstractPositionState } from "../abstract/PositionAbstract";
-import { ICurrentPlayerPosition } from "../interfaces/ClientInterfaces";
 
 export class PositionState implements AbstractPositionState {
 
